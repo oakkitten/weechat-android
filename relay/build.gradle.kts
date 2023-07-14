7cfd4e5583e6e1a01e71b895ad71657812397ea0
oakkitten <oakkitten@users.noreply.github.com>
2023-07-14 13:19:22 +0100

    Bump SDK version to 33 & update libraries
    Bumping to 34 requires more extensive changes

--- relay/build.gradle.kts
+++ relay/build.gradle.kts
@@ -7,18 +7,18 @@ plugins {
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("org.slf4j:slf4j-api:2.0.7")
 
     // "api" because we are calling `SSHConnection.getKnownHosts` from the app
     // and it returns something from inside sshlib
-    api("com.github.connectbot:sshlib:2.2.20")
+    api("com.github.connectbot:sshlib:2.2.21")
 
     implementation("com.neovisionaries:nv-websocket-client:2.14")
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
 }
 
 tasks.withType<JavaCompile> {
