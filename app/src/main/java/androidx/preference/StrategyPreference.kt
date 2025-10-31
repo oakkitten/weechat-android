66dfb3c78cd919e45bf8eb15ac4ab826bd14ea06
oakkitten <oakkitten@users.noreply.github.com>
2025-10-31 19:37:07 +0000

    Raw convert `StrategyPreference` to Kotlin

--- app/src/main/java/androidx/preference/StrategyPreference.kt
+++ app/src/main/java/androidx/preference/StrategyPreference.kt
@@ -1,54 +1,49 @@
-package androidx.preference;
+package androidx.preference
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.widget.TextView;
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+import com.ubergeek42.WeechatAndroid.R
+import com.ubergeek42.WeechatAndroid.media.Config
+import com.ubergeek42.WeechatAndroid.utils.Utils
+import com.ubergeek42.cats.Cat
 
-import com.ubergeek42.WeechatAndroid.R;
-import com.ubergeek42.WeechatAndroid.media.Config;
-import com.ubergeek42.WeechatAndroid.media.Strategy;
-import com.ubergeek42.WeechatAndroid.utils.Utils;
-import com.ubergeek42.cats.Cat;
+class StrategyPreference(context: Context?, attrs: AttributeSet?) :
+        FullScreenEditTextPreference(context, attrs) {
+    @Cat(exit = true) override fun getSummary(): CharSequence? {
+        val context = getContext()
 
-import java.util.ArrayList;
-import java.util.List;
+        val info = Config.parseConfigSafe(getText())
+        if (info == null) return context.getString(R.string.pref__StrategyPreference__summary_error)
 
-public class StrategyPreference extends FullScreenEditTextPreference {
-    public StrategyPreference(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+        val messageFilter =
+            if (info.messageFilter != null) context.getString(R.string.pref__StrategyPreference__message_filter_set) else context.getString(
+                R.string.pref__StrategyPreference__message_filter_not_set)
 
-    @Cat(exit = true) @Override public CharSequence getSummary() {
-        Context context = getContext();
+        val lineFilters =
+            if (info.lineFilters == null) context.getString(R.string.pref__StrategyPreference__0_line_filters_set) else context.getResources()
+                    .getQuantityString(R.plurals.pref__StrategyPreference__n_line_filters_set,
+                                       info.lineFilters!!.size, info.lineFilters!!.size)
 
-        Config.Info info = Config.parseConfigSafe(getText());
-        if (info == null)
-            return context.getString(R.string.pref__StrategyPreference__summary_error);
-
-        String messageFilter = info.messageFilter != null ?
-                context.getString(R.string.pref__StrategyPreference__message_filter_set) :
-                context.getString(R.string.pref__StrategyPreference__message_filter_not_set);
-
-        String lineFilters = info.lineFilters == null ?
-                context.getString(R.string.pref__StrategyPreference__0_line_filters_set) :
-                context.getResources().getQuantityString(R.plurals.pref__StrategyPreference__n_line_filters_set,
-                        info.lineFilters.size(), info.lineFilters.size());
-
-        String summaries;
+        val summaries: String?
         if (info.strategies == null) {
-            summaries = context.getString(R.string.pref__StrategyPreference__strategies_not_loaded);
+            summaries = context.getString(R.string.pref__StrategyPreference__strategies_not_loaded)
         } else {
-            List<CharSequence> names = new ArrayList<>();
-            for (Strategy s : info.strategies) names.add(s.getName());
-            summaries = context.getString(R.string.pref__StrategyPreference__strategies_list, Utils.join(", ", names));
+            val names: MutableList<CharSequence?> = ArrayList<CharSequence?>()
+            for (s in info.strategies) names.add(s.getName())
+            summaries = context.getString(R.string.pref__StrategyPreference__strategies_list,
+                                          Utils.join(", ", names))
         }
 
-        return context.getString(R.string.pref__StrategyPreference__summary, messageFilter, lineFilters, summaries);
+        return context.getString(R.string.pref__StrategyPreference__summary,
+                                 messageFilter,
+                                 lineFilters,
+                                 summaries)
     }
 
-    @Override public void onBindViewHolder(PreferenceViewHolder holder) {
-        super.onBindViewHolder(holder);
-        TextView summary = (TextView) holder.findViewById(android.R.id.summary);
-        summary.setMaxHeight(Integer.MAX_VALUE);
+    override fun onBindViewHolder(holder: PreferenceViewHolder) {
+        super.onBindViewHolder(holder)
+        val summary = holder.findViewById(android.R.id.summary) as TextView
+        summary.setMaxHeight(Int.Companion.MAX_VALUE)
     }
 }
