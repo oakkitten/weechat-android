f0ab3e9629b5c03d82bd4d9a60bf5cd0c7826eab
oakkitten <oakkitten@users.noreply.github.com>
2026-09-05 16:39:24 +0100

    Do not apply serialization plugin in project build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
 
     // to print a sensible task graph, uncomment the following line and run:
